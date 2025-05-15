a60e69717ba7f53ba3e2675ea99d2fce723bcc57
liu.yuqiang <liuyuqiang@sihesci-tech.com>
2025-05-15 16:14:23 +0800

    Update forge.config.js for GitHub publisher

--- forge.config.js
+++ forge.config.js
@@ -46,8 +46,8 @@ module.exports = {
       name: '@electron-forge/publisher-github',
       config: {
         repository: {
-          owner: 'github-user-name',
-          name: 'github-repo-name'
+          owner: 'jarrywen',
+          name: 'my-electron-app'
         },
         prerelease: false,
         draft: true
