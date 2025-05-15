0dccaac1df128d41115a05048eaa9f999378b46f
liu.yuqiang <liuyuqiang@sihesci-tech.com>
2025-05-15 17:25:17 +0800

    update app

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const { app, BrowserWindow, ipcMain } = require('electron/main')
+const { updateElectronApp } = require('update-electron-app')
+console.log('update-electron-app module:', updateElectronApp);
+updateElectronApp({
+  repo: 'jarrywen/my-electron-app', // 替换为你的GitHub仓库路径
+  updateInterval: '1 hour', // 检查更新的频率
+  logger: console // 打印更新日志
+})
+
 const path = require('node:path')
-require('update-electron-app')()
 console.log('Hello from main process!')
 
-console.log('version v1.0.3!')
+console.log('version v1.0.4!')
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
