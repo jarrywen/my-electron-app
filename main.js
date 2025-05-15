9a307062b696c96606b575ac0c0aa64939a84aa1
liu.yuqiang <liuyuqiang@sihesci-tech.com>
2025-05-15 16:08:20 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+const path = require('node:path')
+
+console.log('Hello from main process!')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+  win.webContents.openDevTools(); // 打开开发者工具
+}
+
+app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
